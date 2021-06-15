baa7c602a1fd30e0ec14d1d1fc7c3484aba6fc05
Bradley Buda <bradleybuda@gmail.com>
2021-06-14 21:45:31 -0700

    Add list_fields

--- netlify/functions/postgres.ts
+++ netlify/functions/postgres.ts
@@ -2,16 +2,16 @@ import { Handler } from "@netlify/functions";
 import { Request, Server } from "./protocol";
 import { Connection } from 'postgresql-client';
 
-const server = (connection) => {
+const server = (postgres) => {
   return {
     test_connection: async (request) => {
-      const result = await connection.query('select 1 as one');
+      const result = await postgres.query('select 1 as one');
       const rows = result.rows;
       return { success: true };
     },
 
     list_objects: async (request) => {
-      const result = await connection.query("select table_schema, table_name from information_schema.tables where table_schema NOT IN ('pg_catalog', 'information_schema')");
+      const result = await postgres.query("select table_schema, table_name from information_schema.tables where table_schema NOT IN ('pg_catalog', 'information_schema')");
       console.log(result.rows);
       const objects = result.rows.map(cols => {
         const object_api_name = `${cols[0]}.${cols[1]}`;
@@ -23,6 +23,44 @@ const server = (connection) => {
     supported_operations: async (request) => {
       return {operations: ['insert', 'update', 'upsert']};
     },
+
+    list_fields: async (request) => {
+      const [schema, table] = request.object.object_api_name.split(".");
+
+      const query = `select column_name, data_type from information_schema.columns where table_schema = '${schema}' and table_name = '${table}'`;
+      console.log({query});
+      const result = await postgres.query(query);
+      console.log(result.rows);
+
+      const fields = result.rows.map(cols => {
+        const [column_name, data_type] = cols;
+
+        let field_type;
+        if (data_type == 'character varying') {
+          field_type = 'string';
+        } else if (data_type == 'integer') {
+          field_type = 'integer';
+        } else if (data_type == 'boolean') {
+          field_type = 'boolean';
+        } else {
+          // TODO handle other postgres types
+          field_type = 'string';
+        }
+
+        return {
+          field_api_name: column_name,
+          label: column_name,
+          identifier: true, // TODO depends on index
+          required: true, // TODO depends on index / constraints
+          createable: true,
+          updateable: true,
+          array: false,
+          type: field_type,
+        };
+      });
+
+      return {fields};
+    }
   } as Server;
 }
 
