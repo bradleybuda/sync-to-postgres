4cda4e204a9e81e859ba48d33e07e771d7d9dfed
Bradley Buda <bradleybuda@gmail.com>
2021-06-14 20:44:31 -0700

    Add protocol, tester

--- netlify/functions/postgres.ts
+++ netlify/functions/postgres.ts
@@ -1,9 +1,26 @@
 import { Handler } from "@netlify/functions";
+import { Request, Server } from "./protocol";
+
+const server : Server = {
+  test_connection: async (request) => {
+    return { success: true };
+  }
+};
 
 const handler: Handler = async (event, context) => {
+  const request = JSON.parse(event.body);
+
+  const method = request.method;
+  const result = await server[method](request.params);
+  const response = {
+    jsonrpc: "2.0",
+    id: request.id,
+    result,
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify(response),
   };
 };
 
